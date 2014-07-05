6d3107a9031b9d00948494266ef300dba5b4b854
Korneliusz Caputa <helluinster@gmail.com>
2014-07-05 11:17:55 +0200

    ASCII representation as a reminder

--- src/go/graph_test.go
+++ src/go/graph_test.go
@@ -59,7 +59,14 @@ func TestVertexCoverSimpleGraph(t *testing.T) {
 	assert.Equal(t, g.IsVertexCover(1, 3), true)
 }
 
-func TestVertexCoverComplicatedGraph(t *testing.T) {
+func TestVertexCoverNonTrivialGraph1(t *testing.T) {
+	/*
+		   1o---o2
+			|\ /|
+			| o5|
+			|/ \|
+		   4o---o3
+	*/
 	g := MkGraph()
 	g.AddVertex(1)
 	g.AddVertex(2)
@@ -77,4 +84,40 @@ func TestVertexCoverComplicatedGraph(t *testing.T) {
 
 	assert.Equal(t, g.IsVertexCover(5), false)
 	assert.Equal(t, g.IsVertexCover(1, 3, 5), true)
+}
+
+func TestVertexCoverNonTrivialGraph2(t *testing.T) {
+	/*
+
+		   1o--------o2
+			|\      /|
+			|5o----o6|
+			| |    | |
+			|8o----o7|
+			|/      \|
+		   4o--------o3
+	*/
+	g := MkGraph()
+	g.AddVertex(1)
+	g.AddVertex(2)
+	g.AddVertex(3)
+	g.AddVertex(4)
+	g.AddVertex(5)
+	g.AddVertex(6)
+	g.AddVertex(7)
+	g.AddVertex(8)
+
+	g.AddEdge(1, 2)
+	g.AddEdge(2, 3)
+	g.AddEdge(3, 4)
+	g.AddEdge(1, 5)
+	g.AddEdge(2, 6)
+	g.AddEdge(3, 7)
+	g.AddEdge(4, 8)
+	g.AddEdge(5, 6)
+	g.AddEdge(6, 7)
+	g.AddEdge(7, 8)
+	g.AddEdge(8, 5)
+
+	assert.Equal(t, g.IsVertexCover(2, 3, 4, 5, 7), true)
 }
