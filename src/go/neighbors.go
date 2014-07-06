4de42309d2bf4d2324d8560395d230a9bba5d56a
Korneliusz Caputa <helluinster@gmail.com>
2014-07-06 12:12:30 +0200

    `getNeighbors` bugfix

--- src/go/neighbors.go
+++ src/go/neighbors.go
@@ -4,6 +4,11 @@ type Neighbors []Vertex
 
 func (self *Graph) getNeighbors(v Vertex) Neighbors {
 	result := Neighbors{}
+	coveredEdges := Edges{}
+	for _, i := range self.getCoveredEdgePositions(v) {
+		coveredEdges = append(coveredEdges, self.Edges[i])
+	}
+
 	for i := range self.getCoveredEdgePositions(v) {
 		edge := self.Edges[i]
 		if edge.from != v && !contains(result, edge.from) {
@@ -14,4 +19,12 @@ func (self *Graph) getNeighbors(v Vertex) Neighbors {
 	}
 
 	return result
+}
+
+func (self Neighbors) appendIfNotContains(v Vertex) Neighbors {
+	if !contains(self, v) {
+		self = append(self, v)
+	}
+
+	return self
 }
