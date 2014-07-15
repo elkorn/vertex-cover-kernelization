a0af308771c9b890eaaf74bfebbd206bcff50109
Korneliusz Caputa <helluinster@gmail.com>
2014-07-15 20:26:10 +0200

    Refactoring vertex to support push-degree #1.

--- src/vertex.go
+++ src/vertex.go
@@ -1,18 +1,24 @@
 package graph
 
-type Vertex int
-
-type Node struct {
+type Vertex struct {
 	// This will become Vertex after refactoring.
 	id     int
 	degree int
 }
 
+func mkVertex(id int) Vertex {
+	return Vertex{id, 0}
+}
+
+func (self Vertex) eq(other Vertex) bool {
+	return other != nil && self.id == other.id
+}
+
 func (self *Graph) generateVertex() Vertex {
-	candidate := Vertex(len(self.Vertices))
+	candidate := len(self.Vertices)
 	for self.hasVertex(candidate) {
-		candidate = Vertex(candidate + 1)
+		candidate += 1
 	}
 
-	return candidate
+	return mkVertex(candidate)
 }
