a03e0eed81de034e6f47cb4a834dddb1d1c46d64
Korneliusz Caputa <helluinster@gmail.com>
2014-08-17 17:41:35 +0200

    Move stuff around to keep files small.

---
+++ src/alternating-path.go
@@ -0,0 +1,41 @@
+package graph
+
+import "github.com/deckarep/golang-set"
+
+func isAlternatingPathWithMatching(path []int, matching mapset.Set) bool {
+	previous := MkEdgeFromInts(path[0], path[1])
+	// P is an alternating path if:
+	//  - P is a path in G
+	//  - for each subsequent pair of edges from P one of them
+	//    belongs to M and the other one does not.
+
+	for i := 2; i < len(path); i++ {
+		current := MkEdgeFromInts(path[i-1], path[i])
+		inVerboseContext(func() {
+			Debug("Checking %v:%v", previous, current)
+		})
+
+		if matching.Contains(previous) == matching.Contains(current) {
+			return false
+		}
+
+		previous = current
+	}
+
+	return true
+}
+
+func (from Vertex) isReachableWithMatchingThroughAlternatingPath(to Vertex, net Net, matching mapset.Set) bool {
+	exists, path, _ := shortestPath(net, from, to)
+	if len(path) <= 2 {
+		// TODO I'm not sure whether in case where only a single edge is in the path,
+		// the mere fact of it existing is enough - does it have to belong/not belong to M?
+		return exists
+	}
+
+	if exists {
+		return isAlternatingPathWithMatching(path, matching)
+	}
+
+	return false
+}
