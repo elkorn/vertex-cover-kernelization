9340294bd84ee2e65cc0654c1ccc3f13dc153c5c
Korneliusz Caputa <helluinster@gmail.com>
2014-08-24 20:04:47 +0200

    Add a test for maximal matching.

--- src/matching_test.go
+++ src/matching_test.go
@@ -10,8 +10,16 @@ func TestMaximalMatching(t *testing.T) {
 	g := mkGraph1()
 
 	m, o := maximalMatching(g)
-	assert.Equal(t, 2, m.Cardinality(), "The matching of graph1 should contain only 2 edges.")
-	assert.Equal(t, 6, o.Cardinality(), "The outsiders of graph1 should contain all unmatched edges.")
+	assert.Equal(t, 2, m.Cardinality(), "The matching of graph1 should contain 2 edges.")
+	assert.Equal(t, 6, o.Cardinality(), "The outsiders of graph1 should contain 6 unmatched edges.")
 	assert.True(t, m.Contains(g.getEdgeByCoordinates(0, 1)))
 	assert.True(t, m.Contains(g.getEdgeByCoordinates(2, 3)))
+
+	g = mkGraph5()
+	m, o = maximalMatching(g)
+	assert.Equal(t, 3, m.Cardinality(), "The matching of graph5 should contain 3 edges.")
+	assert.Equal(t, 4, o.Cardinality(), "The outsiders of graph5 should contain 4 unmatched edges.")
+	assert.True(t, m.Contains(g.getEdgeByCoordinates(0, 1)))
+	assert.True(t, m.Contains(g.getEdgeByCoordinates(3, 4)))
+	assert.True(t, m.Contains(g.getEdgeByCoordinates(5, 6)))
 }
