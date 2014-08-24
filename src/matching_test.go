893ce283032fbaf7d2a167ae0b445a7452eae096
Korneliusz Caputa <helluinster@gmail.com>
2014-08-24 20:13:51 +0200

    Add vertex exposition check.

--- src/matching_test.go
+++ src/matching_test.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"testing"
 
+	"github.com/deckarep/golang-set"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,4 +23,23 @@ func TestMaximalMatching(t *testing.T) {
 	assert.True(t, m.Contains(g.getEdgeByCoordinates(0, 1)))
 	assert.True(t, m.Contains(g.getEdgeByCoordinates(3, 4)))
 	assert.True(t, m.Contains(g.getEdgeByCoordinates(5, 6)))
+}
+
+func TestIsExposed(t *testing.T) {
+	matching := mapset.NewSet()
+	matching.Add(MkEdge(1, 2))
+	matching.Add(MkEdge(2, 3))
+	matching.Add(MkEdge(4, 5))
+	matching.Add(MkEdge(7, 8))
+
+	assert.False(t, Vertex(1).isExposed(matching))
+	assert.False(t, Vertex(2).isExposed(matching))
+	assert.False(t, Vertex(3).isExposed(matching))
+	assert.False(t, Vertex(4).isExposed(matching))
+	assert.False(t, Vertex(5).isExposed(matching))
+	assert.False(t, Vertex(7).isExposed(matching))
+	assert.False(t, Vertex(8).isExposed(matching))
+
+	assert.True(t, Vertex(6).isExposed(matching))
+	assert.True(t, Vertex(9).isExposed(matching))
 }
