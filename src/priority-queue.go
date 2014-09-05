5b4751145dfbdd44c842b938dc1ac98929538d05
Korneliusz Caputa <helluinster@gmail.com>
2014-09-05 20:11:53 +0200

    Fix priority queue to actually use the heap correctly. :P

--- src/priority-queue.go
+++ src/priority-queue.go
@@ -10,60 +10,83 @@ type pqItem struct {
 	index int // The index of the item in the heap.
 }
 
-// A PriorityQueue implements heap.Interface and holds pqItems.
-type PriorityQueue []*pqItem
+// A priorityQueue implements heap.Interface and holds pqItems.
+type priorityQueue []*pqItem
 
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Empty() bool { return pq.Len() == 0 }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// If the nodes are at the same level, priority goes to the better lower bound.
-	if pq[i].value.level == pq[j].value.level {
-		return pq[i].value.lowerBound < pq[j].value.lowerBound
-	}
-
-	// Nodes on a deeper level have priority.
-	return pq[i].value.level > pq[j].value.level
-
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	// return pq[i].priority > pq[j].priority
+type PriorityQueueProxy struct {
+	pq *priorityQueue
 }
 
-func (pq PriorityQueue) Swap(i, j int) {
+func MkPriorityQueue() *PriorityQueueProxy {
+	result := &PriorityQueueProxy{
+		pq: new(priorityQueue),
+	}
+
+	heap.Init(result.pq)
+	return result
+}
+
+func (self *PriorityQueueProxy) Push(node *lpNode) {
+	heap.Push(self.pq, &pqItem{
+		value: node,
+	})
+}
+
+func (self *PriorityQueueProxy) Pop() *lpNode {
+	result := heap.Pop(self.pq).(*pqItem)
+	return result.value
+}
+
+func (self *PriorityQueueProxy) Empty() bool {
+	return self.pq.Empty()
+}
+
+func (pq priorityQueue) Len() int { return len(pq) }
+
+func (pq priorityQueue) Empty() bool { return pq.Len() == 0 }
+
+func (pq priorityQueue) Less(i, j int) bool {
+	// If the nodes have the same lower bound (cost),
+	// take the one at the deeper level.
+
+	if pq[i].value.lowerBound == pq[j].value.lowerBound {
+		return pq[i].value.level >= pq[j].value.level
+	}
+
+	// Nodes with the lower cost have priority.
+	return pq[i].value.lowerBound < pq[j].value.lowerBound
+}
+
+func (pq priorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 	pq[i].index = i
 	pq[j].index = j
 }
 
-func (pq *PriorityQueue) Push(x interface{}) {
+func (pq *priorityQueue) Push(x interface{}) {
 	n := len(*pq)
 	item := x.(*pqItem)
+	Debug("Push (%v:%v) %v", item.value.level, item.value.lowerBound, item.value.selection)
 	item.index = n
 	*pq = append(*pq, item)
 }
 
-func (pq *PriorityQueue) PushVal(x interface{}) {
+func (pq *priorityQueue) PushVal(x interface{}) {
 	pq.Push(&pqItem{
 		value: x.(*lpNode),
 	})
 }
 
-func (pq *PriorityQueue) Pop() interface{} {
+func (pq *priorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
+	Debug("Pop (%v:%v) %v", item.value.level, item.value.lowerBound, item.value.selection)
 	item.index = -1 // for safety
 	*pq = old[0 : n-1]
 	return item
 }
 
-func (pq *PriorityQueue) PopVal() interface{} {
+func (pq *priorityQueue) PopVal() interface{} {
 	return pq.Pop().(*pqItem).value
-}
-
-// update modifies the priority and value of an pqItem in the queue.
-func (pq *PriorityQueue) update(item *pqItem, value *lpNode) {
-	item.value = value
-	heap.Fix(pq, item.index)
 }
--- src/priority-queue_test.go
+++ src/priority-queue_test.go
@@ -1,98 +1,38 @@
 package graph
 
 import (
-	"container/heap"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestLevelPriority(t *testing.T) {
-	// Some items and their priorities.
-	n1 := new(lpNode)
-	n2 := new(lpNode)
-	n3 := new(lpNode)
-	n4 := new(lpNode)
-	n1.level = 1
-	n2.level = 2
-	n3.level = 3
-	n4.level = 4
-	items := [3]*lpNode{n1, n2, n3}
-
-	// Create a priority queue, put the items in it, and
-	// establish the priority queue (heap) invariants.
-	pq := make(PriorityQueue, len(items))
-	i := 0
-	for _, value := range items {
-		pq[i] = &pqItem{
-			value: value,
-			index: i,
-		}
-		i++
-	}
-	heap.Init(&pq)
-
-	// Insert a new item and then modify its priority.
-	item := &pqItem{
-		value: n4,
-	}
-
-	heap.Push(&pq, item)
-	pq.update(item, item.value)
-	previouspqItem := &pqItem{
-		value: &lpNode{level: 1000},
-		index: 100,
-	}
-
-	// Take the items out; they arrive in decreasing priority order by level.
-	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*pqItem)
-		assert.True(t, item.value.level < previouspqItem.value.level, "Deeper level should be prioritized.")
-		previouspqItem = item
-	}
-}
 
 func TestLowerBoundPriority(t *testing.T) {
 	n1 := &lpNode{level: 1, lowerBound: 4}
 	n2 := &lpNode{level: 1, lowerBound: 3}
 	n3 := &lpNode{level: 1, lowerBound: 2}
 	n4 := &lpNode{level: 1, lowerBound: 1}
-	items := [3]*lpNode{n1, n2, n3}
+	items := [4]*lpNode{n1, n2, n3, n4}
 
 	// Create a priority queue, put the items in it, and
 	// establish the priority queue (heap) invariants.
-	pq := make(PriorityQueue, len(items))
-	i := 0
+	pq := MkPriorityQueue()
 	for _, value := range items {
-		pq[i] = &pqItem{
-			value: value,
-			index: i,
-		}
-		i++
-	}
-	heap.Init(&pq)
-
-	item := &pqItem{
-		value: n4,
+		pq.Push(value)
 	}
 
-	heap.Push(&pq, item)
-	previouspqItem := &pqItem{
-		value: &lpNode{level: 1, lowerBound: 0},
-		index: 100,
-	}
+	previousLpNode := &lpNode{level: 1, lowerBound: 0}
 
 	// Take the items out, they arrive in an increasing order by lower bound.
-	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*pqItem)
-		assert.True(t, item.value.lowerBound > previouspqItem.value.lowerBound, "Smaller lower bound should be prioritized.")
-		previouspqItem = item
+	for !pq.Empty() {
+		item := pq.Pop()
+		assert.True(t, item.lowerBound > previousLpNode.lowerBound, "Smaller lower bound should be prioritized.")
+		previousLpNode = item
 	}
 }
 
 func TestPopVal(t *testing.T) {
-	q1 := PriorityQueue{}
-	q2 := PriorityQueue{}
+	q1 := priorityQueue{}
+	q2 := priorityQueue{}
 	item1 := &pqItem{
 		value: &lpNode{level: 1},
 	}
@@ -103,7 +43,7 @@ func TestPopVal(t *testing.T) {
 }
 
 func TestPushVal(t *testing.T) {
-	pq := PriorityQueue{}
+	pq := priorityQueue{}
 	item := &lpNode{}
 
 	pq.PushVal(item)
