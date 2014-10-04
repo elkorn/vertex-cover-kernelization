d1ea5c5442619bb9dfed7d05ab492007df1211d6
Korneliusz Caputa <helluinster@gmail.com>
2014-10-04 19:18:31 +0200

    Add 'tests' for the final algorithm.

---
+++ src/chen-kanj-xia_test.go
@@ -0,0 +1,16 @@
+package graph
+
+// TODO: Test this after fixing findCrown. It's slow and crashes.
+// Also, come up with better test scenarios.
+//
+// import (
+// 	"log"
+// 	"testing"
+// )
+//
+// func TestConditionalGeneralFold(t *testing.T) {
+// 	g := ScanGraph("../examples/sh2/sh2-3.dim.sh")
+// 	T := identifyStructures(g, 246)
+// 		_, reduction := conditionalGeneralFold(g, T, nil, 246)
+// 		Debug("Contidional general fold reduction: %v", reduction)
+// }
