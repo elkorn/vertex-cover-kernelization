5a1b269c984ff756968cc6ca4e38d6b26362459c
Korneliusz Caputa <helluinster@gmail.com>
2014-10-10 18:16:48 +0200

    Comment out a test that slows stuff down.

--- graph/chen-kanj-xia_test.go
+++ graph/chen-kanj-xia_test.go
@@ -3,14 +3,19 @@ package graph
 // TODO: Test this after fixing findCrown. It's slow and crashes.
 // Also, come up with better test scenarios.
 //
-// import (
-// 	"log"
-// 	"testing"
-// )
-//
+
 // func TestConditionalGeneralFold(t *testing.T) {
 // 	g := ScanGraph("../examples/sh2/sh2-3.dim.sh")
 // 	T := identifyStructures(g, 246)
-// 		_, kPrime := conditionalGeneralFold(g, T, nil, 246)
-// 		Debug("Contidional general fold reduction: %v", 246 - kPrime)
+// 	instance := &ChenKanjXiaVC{
+// 		G:      g,
+// 		T:      T,
+// 		tuples: mapset.NewSet(),
+// 		k:      246,
+// 		halt:   nil,
+// 	}
+
+// 	InVerboseContext(func() {
+// 	instance.conditionalGeneralFold()
+// 	})
 // }
