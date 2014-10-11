db33a05b63038b7616815b97fb2a5051b426a86f
Korneliusz Caputa <helluinster@gmail.com>
2014-10-11 23:39:44 +0200

    Add pointers as to what should be done <next class=""></next>

--- graph/nt-reduction.go
+++ graph/nt-reduction.go
@@ -2,6 +2,10 @@ package graph
 
 import "github.com/lukpank/go-glpk/glpk"
 
+// TODO: @start-from-here use the Operational Research book to guide yourself
+// through implementing the NT-decomposition.
+// Decide what should be returned (probably 3 sets of vertices)
+
 func ntReduction(g *Graph, k int) {
 	lp := glpk.New()
 	lp.SetProbName("NT decomposition")
