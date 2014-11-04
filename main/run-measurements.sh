0a4ce684b3d6e4ea7e50dfd1c7222e154fccc6c6
Korneliusz Caputa <helluinster@gmail.com>
2014-11-04 05:59:10 +0100

    Change the parameter name for running measurements.

--- main/run-measurements.sh
+++ main/run-measurements.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-go run *.go -run=Bnb &\
-go run *.go -run=Naive &\
-go run *.go -run=NetworkFlow &\
-go run *.go -run=Crown
+go run *.go -measure Bnb &\
+go run *.go -measure NetworkFlow &\
+go run *.go -measure Crown
